148b7b6bdfb38971c9e9e2e1062ae5322924d17e
Joel Hooks <joel@egghead.io>
2019-02-11 11:11:57 -0800

    some stuff I had here on the hard drive

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ app.use(express.static(__dirname + '/_book'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
-app.get('*', function(req, res) {
-  res.redirect('https://howtoegghead.com/instructors', '301')
-})
+// app.get('*', function(req, res) {
+//   res.redirect('https://howtoegghead.com/instructors', '301')
+// })
 
+function redirectUnmatched(req, res) {
+  res.redirect('https://howtoegghead.com/instructors', '301')
+}
+
+app.use(redirectUnmatched)
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'))
 })
